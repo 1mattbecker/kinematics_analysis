55a73c0987989cfabdcaec46c27a14c1ccbeef96
1mattbecker <matt.becker@alleninstitute.org>
2025-09-26 22:39:02 +0000

    finalized for batch

--- code/run_batch_analysis.py
+++ code/run_batch_analysis.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+import json
 from aind_dynamic_foraging_behavior_video_analysis.kinematics.tongue_analysis import run_batch_analysis
-from scratch.pred_csv_list import pred_csv_list
 
 save_root = "/root/capsule/scratch/session_analysis_out_of_distribution_full"
 data_root = Path("/root/capsule/data")
 
-test_list = [pred_csv_list[0]]
+# Load list from JSON
+pred_list_path = Path("/root/capsule/scratch/pred_csv_list.json")
+with open(pred_list_path, "r") as f:
+    pred_csv_list = json.load(f)
 
 run_batch_analysis(pred_csv_list, data_root, save_root)
