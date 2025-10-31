89e7a173d7402f1331c111c8401298d49c0d33c6
1mattbecker <matt.becker@alleninstitute.org>
2025-10-31 19:30:53 +0000

    attach data and analyze with new model

--- code/run_batch_analysis.py
+++ code/run_batch_analysis.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import json
 from aind_dynamic_foraging_behavior_video_analysis.kinematics.tongue_analysis import run_batch_analysis
 
-save_root = "/root/capsule/scratch/session_analysis_out_of_distribution_full"
+save_root = "/root/capsule/scratch/session_analysis_halloween"
 data_root = Path("/root/capsule/data")
 
 # Load list from JSON
-pred_list_path = Path("/root/capsule/scratch/pred_csv_list.json")
+pred_list_path = Path("/root/capsule/scratch/pred_csv_list_halloween.json")
 with open(pred_list_path, "r") as f:
     pred_csv_list = json.load(f)
 
